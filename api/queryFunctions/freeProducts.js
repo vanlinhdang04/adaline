22c192b77f5c455c41755ae53774090b398a58ac
vanlinhdang04 <vanlinhdang1999@gmail.com>
2023-03-29 11:26:36 +0700

    change/Home: refactor api FreeProducts

---
+++ api/queryFunctions/freeProducts.js
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchAPI } from "api/api";
+import { queryKeyList } from "api/queryKeys/queryKeys";
+
+const DEFAULT_STALE_TIME = 10 * 60 * 1000; // 10min
+
+export const fetchFreeProducts = async (
+  path = "/spfrees",
+  params = { populate: "*" },
+  options = {}
+) => {
+  return await fetchAPI(path, params, options);
+};
+
+export const useFetchFreeProducts = (
+  params = { populate: "*" },
+  options = {}
+) => {
+  return useQuery(
+    queryKeyList("/spfrees"),
+    () => fetchFreeProducts("/spfrees", params, options),
+    {
+      staleTime: DEFAULT_STALE_TIME,
+    }
+  );
+};
--- common/components/Home/FreeProducts.js
+++ common/components/Home/FreeProducts.js
@@ -1,3 +1,4 @@
+import { useFetchFreeProducts } from "@/api/queryFunctions/freeProducts";
 import { useFetchProducts } from "@/api/queryFunctions/products";
 import appendImageFromAPI from "@/utils/appendImageFromAPI";
 import {
@@ -16,47 +17,18 @@ import React from "react";
 import HomeTitle from "../HomeTitle";
 
 const FreeProducts = () => {
-  const { data } = useFetchProducts();
-  console.log("data", data);
+  const { data } = useFetchFreeProducts();
 
   return (
     <Box>
       <Center>
         <HomeTitle style={{ textAlign: "center" }}>Phần mềm miễn phí</HomeTitle>
       </Center>
-      <Box>
-        {/* <Grid gutter={"lg"}>
-          {data?.map((product) => (
-            <Grid.Col xs={12} sm={6} md={4} lg={3} key={product.id}>
-              <FreeProductItem product={product} />
-            </Grid.Col>
-          ))}
-        </Grid> */}
-      </Box>
+      <Box></Box>
 
       <Box>
         <Accordion
-          chevron={
-            // <div
-            //   style={{
-            //     height: "100%",
-            //     width: "100%",
-            //     maxHeight: isMobile ? 10 : 14,
-            //     maxWidth: isMobile ? 10 : 14,
-
-            //     float: "right",
-            //   }}
-            // >
-            //   <Image
-            //     src={down}
-            //     alt="ss"
-            //     layout="responsive"
-            //     // height={20}
-            //     // width={20}
-            //   ></Image>
-            // </div>
-            <BsArrowDownRight color="#8eb943" />
-          }
+          chevron={<BsArrowDownRight color="#8eb943" />}
           styles={(theme) => ({
             item: {
               borderRadius: 13,
@@ -107,54 +79,55 @@ const FreeProducts = () => {
           })}
         >
           <Grid gutter={"md"}>
-            {data?.map((product) => (
-              <Grid.Col xs={12} md={6} key={product.id}>
-                <Accordion.Item value={product.attributes.title}>
-                  <Accordion.Control>
-                    <Group
-                      noWrap={true}
-                      spacing="xs"
-                      sx={{
-                        "& h5": {
-                          transition: "var(--transition)",
-                        },
-                        "&:hover": {
-                          h5: {
-                            color: "#8eb943",
+            {Boolean(data) &&
+              data.map((product) => (
+                <Grid.Col xs={12} md={6} key={product.id}>
+                  <Accordion.Item value={product.attributes.siteName}>
+                    <Accordion.Control>
+                      <Group
+                        noWrap={true}
+                        spacing="xs"
+                        sx={{
+                          "& h5": {
+                            transition: "var(--transition)",
                           },
-                        },
-                      }}
-                    >
-                      <Image
-                        src={appendImageFromAPI(
-                          product.attributes.favicon.data.attributes.url
-                        )}
-                        alt="FreeProduct"
-                        width={32}
-                        height={32}
-                      />
-                      <Title order={5} color="#646464" size={14}>
-                        {product.attributes.title}
-                      </Title>
-                    </Group>
-                  </Accordion.Control>
-                  <Accordion.Panel>
-                    <Box>
-                      <Text size={14} color="#767676">
-                        {product.attributes.description}
-                      </Text>
-                      <Link
-                        href={`/product/${product.attributes.slug}_${product.id}`}
+                          "&:hover": {
+                            h5: {
+                              color: "#8eb943",
+                            },
+                          },
+                        }}
                       >
-                        <Text underline color="#8eb943">
-                          Xem thêm
+                        <Image
+                          src={appendImageFromAPI(
+                            product.attributes?.siteIcon?.data?.attributes?.url
+                          )}
+                          alt="FreeProduct"
+                          width={32}
+                          height={32}
+                        />
+                        <Title order={5} color="#646464" size={14}>
+                          {product.attributes.siteName}
+                        </Title>
+                      </Group>
+                    </Accordion.Control>
+                    <Accordion.Panel>
+                      <Box>
+                        <Text size={14} color="#767676">
+                          {product.attributes.siteDescription}
                         </Text>
-                      </Link>
-                    </Box>
-                  </Accordion.Panel>
-                </Accordion.Item>
-              </Grid.Col>
-            ))}
+                        <Link
+                          href={`/product/${product.attributes.siteSlug}_${product.id}`}
+                        >
+                          <Text underline color="#8eb943">
+                            Xem thêm
+                          </Text>
+                        </Link>
+                      </Box>
+                    </Accordion.Panel>
+                  </Accordion.Item>
+                </Grid.Col>
+              ))}
           </Grid>
         </Accordion>
       </Box>
